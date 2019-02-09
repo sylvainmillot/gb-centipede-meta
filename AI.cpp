35b97ba2ceb6265616a93a7a9c652f1750c8b088
Sylvain Millot <sylvain@hastatis.com>
2019-02-09 10:22:27 +0100

    fixing centipede movements bugs

--- AI.cpp
+++ AI.cpp
@@ -20,10 +20,84 @@ void moveBullets(Player &player) {
 }
 
 void moveCentipede(CentipedeBlock * centipede, Mushroom * mushrooms) {
+  // For each Centipede block
   for(int i=0; i<NB_BLOCKS; i++) {
-    bool dontMove = false;
-    
-    if((centipede[i].direction.y > 0 && centipede[i].point.y + CENTIPEDE_SIZE + 1 >= gb.display.height())
+    bool dontMoveX = false;
+
+    // If the Centipede hits left or right side of the screen
+    if(xCollision(centipede, mushrooms, i)) {
+      // We change its "x" direction
+      centipede[i].direction.x = -centipede[i].direction.x;
+
+      // If the Centipede hits top or bottom of the screen 
+      if(yCollision(centipede, mushrooms, i)) {
+        // We change its "y" direction
+        centipede[i].direction.y = -centipede[i].direction.y;
+
+        // Don't move in "x" direction the next frame (seems it's not working?)
+        dontMoveX = true;
+      }
+
+      centipede[i].point.y+= centipede[i].direction.y * CENTIPEDE_SPEED_Y;
+    }
+
+    if(!dontMoveX) {
+      centipede[i].point.x+= centipede[i].direction.x * CENTIPEDE_SPEED_X;
+    }
+  }
+}
+
+bool xCollision(CentipedeBlock * centipede, Mushroom * mushrooms, int i) {
+  bool screenCollision = false;
+  bool centipedeCollision = false;
+  bool mushroomCollision = false;
+
+  // We first check screen collision
+  screenCollision = ((centipede[i].direction.x > 0 && centipede[i].point.x + CENTIPEDE_SIZE + 1 >= gb.display.width()) || (centipede[i].direction.x < 0 && centipede[i].point.x - 1 <= 0));
+
+  if(!screenCollision) {
+    // Then, mushroom collision
+    if(centipede[i].direction.x > 0) {
+      for(int j = 0; j < NB_MUSHROOMS_MAX; j++) {
+        if(mushrooms[j].life > 0) {
+          if(centipede[i].point.x + CENTIPEDE_SIZE + 1 >= mushrooms[j].point.x && centipede[i].point.x + CENTIPEDE_SIZE + 1 <= mushrooms[j].point.x + CENTIPEDE_SIZE && centipede[i].point.y == mushrooms[j].point.y) {
+            mushroomCollision = true;
+          }
+        } 
+      }
+    }else {
+      for(int j = 0; j < NB_MUSHROOMS_MAX; j++) {
+        if(mushrooms[j].life > 0) {
+          if(centipede[i].point.x - 1 >= mushrooms[j].point.x && centipede[i].point.x - 1 <= mushrooms[j].point.x + CENTIPEDE_SIZE && centipede[i].point.y == mushrooms[j].point.y) {
+            mushroomCollision = true;
+          }
+        } 
+      }
+    }
+
+    if(!mushroomCollision) {
+      // Then, Centipede collision
+
+      // Source of bugs, maybe not necessary?
+    }
+  }
+  
+  return screenCollision || centipedeCollision || mushroomCollision;
+}
+
+bool yCollision(CentipedeBlock * centipede, Mushroom * mushrooms, int i) {
+  bool screenCollision = false;
+  bool centipedeCollision = false;
+  bool mushroomCollision = false;
+  
+  screenCollision = ((centipede[i].direction.y > 0 && centipede[i].point.y + CENTIPEDE_SIZE + 1 >= gb.display.height()) || (centipede[i].direction.y < 0 && centipede[i].point.y - 1 <= 0));
+
+  return screenCollision || centipedeCollision || mushroomCollision;
+}
+
+
+/*void old() {
+  if((centipede[i].direction.y > 0 && centipede[i].point.y + CENTIPEDE_SIZE + 1 >= gb.display.height())
     || (centipede[i].direction.y < 0 && centipede[i].point.y - 1 <= 0)) {
       centipede[i].direction.y = -centipede[i].direction.y;
       centipede[i].point.y+= centipede[i].direction.y * CENTIPEDE_SPEED_Y;
@@ -87,8 +161,5 @@ void moveCentipede(CentipedeBlock * centipede, Mushroom * mushrooms) {
       }
     }
 
-    if(!dontMove) {
-      centipede[i].point.x+= centipede[i].direction.x * CENTIPEDE_SPEED_X;
-    }
-  }
-}
+}*/
+
--- AI.h
+++ AI.h
@@ -5,3 +5,5 @@
 void runAI(Player &player, CentipedeBlock * centipede, Mushroom * mushrooms);
 void moveBullets(Player &player);
 void moveCentipede(CentipedeBlock * centipede, Mushroom * mushrooms);
+bool xCollision(CentipedeBlock * centipede, Mushroom * mushrooms, int i);
+bool yCollision(CentipedeBlock * centipede, Mushroom * mushrooms, int i);
